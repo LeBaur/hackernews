1c547cb33c7145f4f681f0f47962106235818ffb
Adilkhanov Bauyrzhan <abaurd@gmail.com>
2014-07-27 22:03:20 +0600

    switched to using named routes

--- hackernews/urls.py
+++ hackernews/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'', include('stories.urls')),
 )
--- stories/urls.py
+++ stories/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'hackernews.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'stories.views.index'),
-    url(r'^story/$', 'stories.views.story'),
-    url(r'^vote/$', 'stories.views.vote'),
+    url(r'^$', 'stories.views.index', name='index'),
+    url(r'^story/$', 'stories.views.story', name='story'),
+    url(r'^vote/$', 'stories.views.vote', name='vote'),
 )
